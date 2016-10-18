e5a47896f1e916f2fc4fe099c61d138d63eeacda
Dmitrii <DPKuznechenkov@1cbit.ru>
2016-10-18 18:39:24 +0300

    Добавлен шаблон экрана

--- SuperService/Controllers/AddTaskScreen.cs
+++ SuperService/Controllers/AddTaskScreen.cs
@@ -1,4 +1,5 @@
 ﻿using BitMobile.ClientModel3.UI;
+using System;
 
 namespace Test
 {
@@ -7,5 +8,24 @@ namespace Test
         public override void OnLoading()
         {
         }
+
+        internal void TopInfo_LeftButton_OnClick(object sender, EventArgs eventArgs)
+        {
+            Navigation.Back();
+        }
+
+        internal void TopInfo_RightButton_OnClick(object sender, EventArgs eventArgs)
+        {
+        }
+
+        internal void TopInfo_Arrow_OnClick(object sender, EventArgs eventArgs)
+        {
+        }
+
+        internal string GetResourceImage(object tag)
+            => ResourceManager.GetImage($"{tag}");
+
+        internal void CreateTask_OnClick(object sender, EventArgs e)
+        {
+        }
     }
-}
