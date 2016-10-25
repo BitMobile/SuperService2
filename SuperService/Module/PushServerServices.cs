f4deebd88585ab358a43019fedb03150cd9d2f3e
LieOff <IIObmanov@1cbit.ru>
2016-10-25 15:47:27 +0300

    PushFix
    PushFix

--- SuperService/Module/PushServerServices.cs
+++ SuperService/Module/PushServerServices.cs
@@ -1,17 +1,20 @@
-﻿using ClientModel3.MD;
+﻿using System;
+using ClientModel3.MD;
 
 namespace Test.Module
 {
-    internal static class PushServerServices
+    internal class PushServerServices
     {
         public static void Init()
         {
+            var userId = Settings.UserDetailedInfo.Id.Guid;
             Utils.TraceMessage($"Push Initialized: {PushNotification.IsInitialized}");
             if (PushNotification.IsInitialized) return;
+            Utils.TraceMessage($"Сервер:{Settings.PushServer} Юзер:{Settings.UserDetailedInfo.Id.Guid} Пароль:{Settings.Password}");
             if (!string.IsNullOrEmpty(Settings.User) && !string.IsNullOrEmpty(Settings.Password) &&
-                !string.IsNullOrEmpty(Settings.PushServer) && !string.IsNullOrEmpty(Settings.UserId))
+                !string.IsNullOrEmpty(Settings.PushServer) && (userId != Guid.Empty))
             {
-                PushNotification.InitializePushService(Settings.PushServer, Settings.UserId, Settings.Password);
+                PushNotification.InitializePushService(Settings.PushServer, userId.ToString(), Settings.Password);
             }
         }
     }
