6d773aea6e4d12afcbd46ad4a2c4ac5584eb76c9
Dmitrii <dpkuznechenkov@1cbit.ru>
2016-12-06 18:53:36 +0300

    Убрал кнопки, осталось одно поле комментарий

--- SuperService/Controllers/CloseEventScreen.cs
+++ SuperService/Controllers/CloseEventScreen.cs
@@ -11,37 +11,14 @@ namespace Test
 {
     public class CloseEventScreen : Screen
     {
-        private bool _problem;
-        private HorizontalLayout _problemButton;
-        private VerticalLayout _problemCommentLayout;
-        private Image _problemImage;
-        private bool _wantToBuy;
-        private HorizontalLayout _wantToBuyButton;
-        private VerticalLayout _wantToBuyCommentLayout;
-        private Image _wantToBuyImage;
-
-        private MemoEdit _wantToBuyCommentMemoEdit;
-        private MemoEdit _problemCommentMemoEdit;
         private MemoEdit _commentaryMemoEdit;
         private TextView _closeResult;
         private static EventResults _eventResults;
 
         private static string _commentaryString;
-        private static string _wantToBuyString;
-        private static string _problemCommentString;
 
         public override void OnLoading()
         {
-            _wantToBuyButton = (HorizontalLayout)GetControl("WantToBuyButton", true);
-            _wantToBuyCommentLayout = (VerticalLayout)GetControl("WantToBuyCommentLayout", true);
-            _wantToBuyImage = (Image)GetControl("WantToBuyImage", true);
-
-            _problemButton = (HorizontalLayout)GetControl("ProblemButton", true);
-            _problemCommentLayout = (VerticalLayout)GetControl("ProblemCommentLayout", true);
-            _problemImage = (Image)GetControl("ProblemImage", true);
-
-            _wantToBuyCommentMemoEdit = (MemoEdit)GetControl("WantToBuyCommentMemoEdit", true);
-            _problemCommentMemoEdit = (MemoEdit)GetControl("ProblemCommentMemoEdit", true);
             _commentaryMemoEdit = (MemoEdit)GetControl("CommentaryMemoEdit", true);
             _closeResult = (TextView)GetControl("75097b50211a453883f5e31f4e878c2c", true);
 
@@ -51,16 +28,6 @@ namespace Test
             {
                 Utils.TraceMessage($"Стока {_commentaryString} пустая");
                 _commentaryString = string.Empty;
-            }
-            if (_wantToBuyString == null)
-            {
-                Utils.TraceMessage($"Стока {_wantToBuyString} пустая");
-                _wantToBuyString = string.Empty;
-            }
-            if (_problemCommentString == null)
-            {
-                Utils.TraceMessage($"Стока {_problemCommentString} пустая");
-                _problemCommentString = string.Empty;
             }
         }
 
@@ -80,35 +47,15 @@ namespace Test
         private void InitComponents()
         {
             _commentaryMemoEdit.Text = _commentaryString;
-            _wantToBuyCommentMemoEdit.Text = _wantToBuyString;
-            _problemCommentMemoEdit.Text = _problemCommentString;
 
             _closeResult.Text = string.IsNullOrEmpty(_eventResults.Description)
                 ? Translator.Translate("not_choosed")
                 : _eventResults.Description;
-
-            Utils.TraceMessage($"_commentMemoEdit.Text {_commentaryMemoEdit.Text}{Environment.NewLine}" +
-                               $"{nameof(_commentaryString)} {_commentaryString}{Environment.NewLine}" +
-                               $"{nameof(_wantToBuyCommentMemoEdit)}.Text {_wantToBuyCommentMemoEdit.Text}{Environment.NewLine}" +
-                               $"{nameof(_wantToBuyString)} {_wantToBuyString}{Environment.NewLine}" +
-                               $"{nameof(_problemCommentMemoEdit)}.Text {_problemCommentMemoEdit.Text}{Environment.NewLine}" +
-                               $"{nameof(_problemCommentString)}");
         }
 
         internal void WantToBuyButton_OnClick(object sender, EventArgs eventArgs)
         {
-            if (!_wantToBuy)
-            {
-                UpdateButtonCSS(_wantToBuyButton, _wantToBuyCommentLayout, _wantToBuyImage, "BigButtonPressed",
-                    "CommentLayout", "closeevent_wtb_selected");
-                _wantToBuy = true;
-            }
-            else
-            {
-                UpdateButtonCSS(_wantToBuyButton, _wantToBuyCommentLayout, _wantToBuyImage, "BigButton", "NoHeight",
-                    "closeevent_wtb");
-                _wantToBuy = false;
-            }
+            
         }
 
         private void UpdateButtonCSS(HorizontalLayout buttonLayout, VerticalLayout commentLayout, Image image,
@@ -125,18 +72,7 @@ namespace Test
 
         internal void ProblemButton_OnClick(object sender, EventArgs eventArgs)
         {
-            if (!_problem)
-            {
-                UpdateButtonCSS(_problemButton, _problemCommentLayout, _problemImage, "BigButtonPressed",
-                    "CommentLayout", "closeevent_problem_selected");
-                _problem = true;
-            }
-            else
-            {
-                UpdateButtonCSS(_problemButton, _problemCommentLayout, _problemImage, "BigButton", "NoHeight",
-                    "closeevent_problem");
-                _problem = false;
-            }
+            
         }
 
         internal void FinishButton_OnClick(object sender, EventArgs eventArgs)
@@ -161,18 +97,7 @@ namespace Test
             var entitiesList = new ArrayList();
             var @event = (Event)eventRef.GetObject();
 
-            if (_wantToBuy)
-            {
-                var reminder = CreateReminder(eventRef, _wantToBuyCommentMemoEdit.Text);
-                reminder.ViewReminder = FoReminders.GetDbRefFromEnum(FoRemindersEnum.Sale);
-                entitiesList.Add(reminder);
-            }
-            if (_problem)
-            {
-                var reminder = CreateReminder(eventRef, _problemCommentMemoEdit.Text);
-                reminder.ViewReminder = FoReminders.GetDbRefFromEnum(FoRemindersEnum.Problem);
-                entitiesList.Add(reminder);
-            }
+          
 
             if (!string.IsNullOrEmpty(_commentaryMemoEdit.Text))
                 @event.CommentContractor = _commentaryMemoEdit.Text;
@@ -186,7 +111,6 @@ namespace Test
             DBHelper.SaveEntities(entitiesList);
 
             _eventResults = null;
-            _commentaryString = _wantToBuyString = _problemCommentString = null;
 
             Navigation.CleanStack();
             Navigation.ModalMove("EventListScreen");
@@ -211,8 +135,6 @@ namespace Test
         internal void CloseEvent_OnClick(object sender, EventArgs e)
         {
             _commentaryString = _commentaryMemoEdit.Text;
-            _wantToBuyString = _wantToBuyCommentMemoEdit.Text;
-            _problemCommentString = _problemCommentMemoEdit.Text;
             Navigation.ModalMove(nameof(CompleteResultScreen));
         }
 
