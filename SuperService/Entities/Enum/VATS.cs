191b81b2f278299d7f7e106eec1d891a409161e9
Dmitrii <dpkuznechenkov@1cbit.ru>
2017-01-26 16:31:49 +0300

    YOU

--- SuperService/Entities/Enum/VATS.cs
+++ SuperService/Entities/Enum/VATS.cs
@@ -8,7 +8,6 @@ namespace Test.Enum
         public DbRef Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int NumberFR { get; set; }
         public static DbRef GetDbRefFromEnum(VATSEnum @enum)
         {
             string res = null;
