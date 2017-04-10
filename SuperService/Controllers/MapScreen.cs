87c78a959b02ac65c75810d5db91390e31c52ea6
Dmitriy Delov <DVDelov@1cbit.ru>
2017-04-10 14:40:19 +0300

    Add changes for MapScreen

--- SuperService/Controllers/MapScreen.cs
+++ SuperService/Controllers/MapScreen.cs
@@ -26,6 +26,7 @@ namespace Test
 
         public override void OnLoading()
         {
+            base.OnLoading();
             _map = (WebMapGoogle)GetControl("Map", true);
             _topInfoComponent = new TopInfoComponent(this)
             {
@@ -39,13 +40,21 @@ namespace Test
 
         public override void OnShow()
         {
-            GpsTracking.Start();
+            base.OnShow();
+            GpsTracking.StartAsync();
             FillMap();
+        }
+
+        public override void OnDraw()
+        {
+            base.OnDraw();
+            Dialog.HideProgressDialog();
         }
 
         internal void TopInfo_LeftButton_OnClick(object sender, EventArgs e)
         {
             DConsole.WriteLine("Back to screen .....");
+            Dialog.ShowProgressDialog(Translator.Translate("loading_message"), true);
             Navigation.Back();
         }
 
@@ -56,6 +65,26 @@ namespace Test
         internal void TopInfo_Arrow_OnClick(object sender, EventArgs e)
         {
             _topInfoComponent.Arrow_OnClick(sender, e);
+        }
+
+        internal void TopInfo_LeftButton_OnPressDown(object sender, EventArgs e)
+        {
+            ((Image)_topInfoComponent.LeftButtonControl).Source = ResourceManager.GetImage("topheading_back_active");
+            _topInfoComponent.Refresh();
+        }
+
+        internal void TopInfo_LeftButton_OnPressUp(object sender, EventArgs e)
+        {
+            ((Image)_topInfoComponent.LeftButtonControl).Source = ResourceManager.GetImage("topheading_back");
+            _topInfoComponent.Refresh();
+        }
+
+        internal void TopInfo_RightButton_OnPressDown(object sender, EventArgs e)
+        {
+        }
+
+        internal void TopInfo_RightButton_OnPressUp(object sender, EventArgs e)
+        {
         }
 
         internal string GetResourceImage(string tag)
